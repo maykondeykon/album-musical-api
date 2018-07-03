2116908cef1f79468a33f44153c349162794331b
Maykon Deykon <maykondeykon@gmail.com>
2018-07-03 20:55:44 -0300

    Atualizações das migrações

--- src/main/resources/db/migration/mysql/V10__insert_musica_album.sql
+++ src/main/resources/db/migration/mysql/V10__insert_musica_album.sql
@@ -1,2 +1,17 @@
 INSERT INTO musica_album (id_musica, id_album) VALUES (1, 1);
-INSERT INTO musica_album (id_musica, id_album) VALUES (2, 4);
+INSERT INTO musica_album (id_musica, id_album) VALUES (2, 1);
+INSERT INTO musica_album (id_musica, id_album) VALUES (3, 1);
+INSERT INTO musica_album (id_musica, id_album) VALUES (4, 1);
+
+INSERT INTO musica_album (id_musica, id_album) VALUES (5, 2);
+INSERT INTO musica_album (id_musica, id_album) VALUES (6, 2);
+INSERT INTO musica_album (id_musica, id_album) VALUES (7, 2);
+INSERT INTO musica_album (id_musica, id_album) VALUES (8, 2);
+
+INSERT INTO musica_album (id_musica, id_album) VALUES (9, 3);
+INSERT INTO musica_album (id_musica, id_album) VALUES (10, 3);
+INSERT INTO musica_album (id_musica, id_album) VALUES (11, 3);
+INSERT INTO musica_album (id_musica, id_album) VALUES (12, 3);
+INSERT INTO musica_album (id_musica, id_album) VALUES (13, 3);
+INSERT INTO musica_album (id_musica, id_album) VALUES (14, 3);
+INSERT INTO musica_album (id_musica, id_album) VALUES (15, 3);
--- src/main/resources/db/migration/mysql/V6__insert_musicas.sql
+++ src/main/resources/db/migration/mysql/V6__insert_musicas.sql
@@ -1,2 +1,17 @@
-INSERT INTO musica (nome, duracao) VALUES ('Midnight Rider', 3);
-INSERT INTO musica (nome, duracao) VALUES ('Eagle fly free', 4);
+INSERT INTO musica (nome, duracao) VALUES ('Dark Forest', 3);
+INSERT INTO musica (nome, duracao) VALUES ('Lost Lady', 4);
+INSERT INTO musica (nome, duracao) VALUES ('Men of Honor', 7);
+INSERT INTO musica (nome, duracao) VALUES ('Midnight Rider', 5);
+
+INSERT INTO musica (nome, duracao) VALUES ('March for the Glory', 6);
+INSERT INTO musica (nome, duracao) VALUES ('Black Cherub', 4);
+INSERT INTO musica (nome, duracao) VALUES ('Midnight Rider - V2', 6);
+INSERT INTO musica (nome, duracao) VALUES ('Heavenly Warrior', 4);
+
+INSERT INTO musica (nome, duracao) VALUES ('Initiation', 1);
+INSERT INTO musica (nome, duracao) VALUES ('Im Alive', 3);
+INSERT INTO musica (nome, duracao) VALUES ('Twilight of the Gods', 4);
+INSERT INTO musica (nome, duracao) VALUES ('A tale that wasnt right', 4);
+INSERT INTO musica (nome, duracao) VALUES ('Future World', 4);
+INSERT INTO musica (nome, duracao) VALUES ('Halloween', 13);
+INSERT INTO musica (nome, duracao) VALUES ('Follow the Sign', 1);
