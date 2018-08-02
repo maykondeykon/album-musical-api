3972955693897bb3c49cebe92b3a360897e45b53
Maykon Deykon <maykondeykon@gmail.com>
2018-08-02 20:41:32 -0300

    Adicionadas informações iniciais à documentação do Swagger

--- src/main/java/com/mkdk/albummusicalapi/config/SwaggerConfig.java
+++ src/main/java/com/mkdk/albummusicalapi/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.mkdk.albummusicalapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +20,16 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Album Musical API")
+                .description("\"API para gerenciamento de álbuns musicais\"")
+                .version("0.1.0")
+                .contact(new Contact("Maykon Deykon", "http://maykondeykon.github.io", "maykondeykon@gmail.com"))
                 .build();
     }
 }
